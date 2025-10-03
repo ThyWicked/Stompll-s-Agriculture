98f0c9c85e68517511618d088db63be398764526
T0X1CCR34T0R <111434313+T0X1CCR34T0R@users.noreply.github.com>
2025-10-04 00:41:36 +0200

    Added ModBlocks Class

--- src/main/java/net/wicked/moreagriculture/datagen/ThisBlockModelGenerator.java
+++ src/main/java/net/wicked/moreagriculture/datagen/ThisBlockModelGenerator.java
@@ -3,8 +3,14 @@ package net.wicked.moreagriculture.datagen;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelOutput;
 import net.minecraft.client.data.models.blockstates.BlockModelDefinitionGenerator;
+import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelInstance;
+import net.minecraft.client.data.models.model.ModelLocationUtils;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.RegistryObject;
+import net.wicked.moreagriculture.block.ModBlocks;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -12,5 +18,15 @@ import java.util.function.Consumer;
 public class ThisBlockModelGenerator extends BlockModelGenerators {
     public ThisBlockModelGenerator(Consumer<BlockModelDefinitionGenerator> pBlockStateOutput, ItemModelOutput pItemModelOutput, BiConsumer<ResourceLocation, ModelInstance> pModelOutput) {
         super(pBlockStateOutput, pItemModelOutput, pModelOutput);
+    }
+
+    @Override
+    public void run() {
+        for (RegistryObject<Block> entry : ModBlocks.getBuildingBlocks().getEntries()) {
+            this.createTrivialCube(entry.get());
+        }
+        ModBlocks.getBuildingBlockItems().getEntries().forEach((item) -> {
+            itemModelOutput.accept(item.get(), ItemModelUtils.plainModel(ModelLocationUtils.getModelLocation(((BlockItem) item.get()).getBlock())));
+        });
     }
 }
